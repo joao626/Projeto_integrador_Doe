0a68cbb7c4c9c3eb11ba3eddfb77f49c9fbab8f0
Rodrigo de Oliveira <rodrigoleao.ci@gmail.com>
2023-07-29 14:45:45 -0300

    primeiro commit spint 1

---
+++ js/javaScript.js
@@ -0,0 +1,13 @@
+document.querySelectorAll('[nav]').forEach(link => {
+const conteudo = document.getElementById('conteudo')
+
+link.onclick = e => {
+    e.preventDefault()
+    fetch(link.getAttribute('nav'))
+        .then(res => res.text())
+        .then(html => conteudo.innerHTML = html)
+    }
+})
+
+
+        
