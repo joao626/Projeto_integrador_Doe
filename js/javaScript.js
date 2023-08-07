b91aa375077bd60a9038d65107aa44e92ce7fcf2
Rodrigo de Oliveira <rodrigoleao.ci@gmail.com>
2023-08-07 16:59:04 -0300

    desativando codigo para estruturar html e css

--- js/javaScript.js
+++ js/javaScript.js
@@ -1,13 +1,13 @@
-document.querySelectorAll('[nav]').forEach(link => {
-const conteudo = document.getElementById('conteudo')
+// document.querySelectorAll('[nav]').forEach(link => {
+// const conteudo = document.getElementById('conteudo')
 
-link.onclick = e => {
-    e.preventDefault()
-    fetch(link.getAttribute('nav'))
-        .then(res => res.text())
-        .then(html => conteudo.innerHTML = html)
-    }
-})
+// link.onclick = e => {
+//     e.preventDefault()
+//     fetch(link.getAttribute('nav'))
+//         .then(res => res.text())
+//         .then(html => conteudo.innerHTML = html)
+//     }
+// })
 
 
         
