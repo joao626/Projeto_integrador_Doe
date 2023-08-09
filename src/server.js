be4d4ba7c384749491ff5b0d581e745c294fda31
Rodrigo de Oliveira <rodrigoleao.ci@gmail.com>
2023-08-09 16:13:11 -0300

    atualizando a arquitetura de pastas...finalizando estrutura html para apresentação sprint 2

---
+++ src/server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended: true}))
+
+app.post('/usuarios', (req, res, next) => {
+    console.log(req.body)
+    res.send('<h1>Parabéns, usuario incluido !</h1>')
+})
+
+app.listen(3003)
