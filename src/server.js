e3425fc5a0796bdebaf561068d5f87a044411c32
Rodrigo de Oliveira <rodrigoleao.ci@gmail.com>
2023-08-27 16:03:13 -0300

    corrigindo...

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const path = require('path');
 
 const bodyParser = require('body-parser')
 
@@ -8,6 +9,8 @@ app.use(bodyParser.urlencoded({ extended: true}))
 app.post('/usuarios', (req, res, next) => {
     console.log(req.body)
     res.send('<h1>Sucesso !</h1>')
+    
 })
+
 
 app.listen(3003)
